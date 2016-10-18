739aea17b644cfd5cb5b79f4c37b170b913c82d7
chrisarg0 <cargonish22@gmail.com>
2016-10-18 15:55:19 -0800

    Added Basic Flows

---
+++ Narcan App/RequestViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RequestViewController.swift
+//  Narcan App
+//
+//  Created by Chris Argonish on 10/18/16.
+//  Copyright © 2016 Chris. All rights reserved.
+//
+
+import UIKit
+
+class RequestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
