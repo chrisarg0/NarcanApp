6d70267c27455e37fb17cd576580edd9ae6b968a
chrisarg0 <cargonish22@gmail.com>
2016-10-26 12:38:37 -0700

    added new assets; updated login screen with new branding

--- Narcan App/AppDelegate.swift
+++ Narcan App/AppDelegate.swift
@@ -44,7 +44,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let navigationBarAppearace = UINavigationBar.appearance()
         
         navigationBarAppearace.tintColor = UIColor.white
-        navigationBarAppearace.barTintColor = UIColor(red:0.90, green:0.06, blue:0.47, alpha:1.00)
+        navigationBarAppearace.barTintColor = UIColor(red:0.16, green:0.29, blue:0.50, alpha:1.00)
         
         UIApplication.shared.statusBarStyle = UIStatusBarStyle.lightContent
         
