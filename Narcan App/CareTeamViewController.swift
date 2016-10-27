ef126cadc75a6ab5fa99aaa7500ceedc3f10a9db
chrisarg0 <cargonish22@gmail.com>
2016-10-27 01:18:32 -0700

    updates to settings pages in storyboard

---
+++ Narcan App/CareTeamViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CareTeamViewController.swift
+//  Narcan App
+//
+//  Created by Chris Argonish on 10/27/16.
+//  Copyright © 2016 Chris. All rights reserved.
+//
+
+import UIKit
+
+class CareTeamViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
