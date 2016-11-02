4cebcce32b3735b40bcaac9a9a568534c5251d97
alexleon8964 <alexleon8964@gmailL.com>
2016-11-02 09:21:41 +0800

    Care Team adding

--- Narcan App/CareTeamViewController.swift
+++ Narcan App/CareTeamViewController.swift
@@ -8,12 +8,19 @@
 
 import UIKit
 
-class CareTeamViewController: UIViewController {
+class CareTeamViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +28,31 @@ class CareTeamViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return AppDelegate.defaultManager.mCareUsers.count
+    }
     
-
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath) 
+        let nameLbl = cell.viewWithTag(100) as! UILabel
+        let roleLbl = cell.viewWithTag(101) as! UILabel
+        
+        let care = AppDelegate.defaultManager.mCareUsers.object(at: indexPath.row) as! NarcanCare
+        nameLbl.text = "\(care.first_name!) \(care.last_name!)"
+        roleLbl.text = "\(care.role!)"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    
+    
+    // Override to support conditional editing of the table view.
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return false
+    }
 }
