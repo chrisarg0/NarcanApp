122439491c76e1276ac273bd40323c54210b7a4a
chrisarg0 <cargonish22@gmail.com>
2016-11-14 15:41:05 -0700

    organized file structure

--- Narcan App/KnowledgeHomeViewController.swift
+++ Narcan App/KnowledgeHomeViewController.swift
@@ -20,7 +20,7 @@ class KnowledgeHomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("hello world")
         // Do any additional setup after loading the view.
     }
 
